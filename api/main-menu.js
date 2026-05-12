d8a50bb3a47ff12685621594dbef43fea2fb46fa
Sam's in Japan <saminjapan100@gmail.com>
2026-05-11 22:37:34 -0400

    Update main-menu.js

--- api/main-menu.js
+++ api/main-menu.js
@@ -2,9 +2,9 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Play>https://us-central-1.telnyxcloudstorage.com/fcgcaudio/audio.mp3</Play>
+  <Play>https://fci-ivr.vercel.app/welcome.mp3</Play>
   <Gather action="https://fci-ivr.vercel.app/api/menu" method="POST" numDigits="1" timeout="5">
-    <Play>https://us-central-1.telnyxcloudstorage.com/fcgcaudio/audio.mp3</Play>
+    <Play>https://fci-ivr.vercel.app/welcome.mp3</Play>
   </Gather>
   <Hangup/>
 </Response>`);
