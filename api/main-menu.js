7ce6c81de9efecf426737c41728e3b671d85954f
Sam's in Japan <saminjapan100@gmail.com>
2026-05-11 21:35:37 -0400

    Update audio file URLs in main-menu.js

--- api/main-menu.js
+++ api/main-menu.js
@@ -2,9 +2,9 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Play>https://your-storage-url.com/audio/welcome.mp3</Play>
+  <Play>https://us-central-1.telnyxcloudstorage.com/fcgcaudio/audio.mp3</Play>
   <Gather action="https://fci-humz3ue19-sams-projects-e77bff13.vercel.app/api/menu" method="POST" numDigits="1" timeout="5">
-    <Play>https://your-storage-url.com/audio/menu.mp3</Play>
+    <Play>https://us-central-1.telnyxcloudstorage.com/fcgcaudio/audio.mp3</Play>
   </Gather>
   <Hangup/>
 </Response>`);
