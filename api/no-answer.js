cd0e216cdd9970fa13532a9a6351826f7dbfe775
Sam's in Japan <saminjapan100@gmail.com>
2026-05-11 21:40:10 -0400

    Update XML response to only include Hangup element
    Remove Play and Record elements from XML response.

--- api/no-answer.js
+++ api/no-answer.js
@@ -2,7 +2,6 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Play>https://your-storage-url.com/audio/corp_voicemail.mp3</Play>
-  <Record maxLength="120" finishOnKey="#" transcribe="true"/>
+  <Hangup/>
 </Response>`);
 }
